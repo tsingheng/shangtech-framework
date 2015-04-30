bcaafbda9d6232c3f1d1f7f4b6f39d304648e24f
tsingheng <tsingheng@163.com>
2015-04-30 16:55:23 +0800

    add attribute countRef to SqlHolder

--- src/main/java/net/shangtech/framework/orm/dao/support/SqlHolder.java
+++ src/main/java/net/shangtech/framework/orm/dao/support/SqlHolder.java
@@ -1,17 +1,24 @@
 package net.shangtech.framework.orm.dao.support;
 
 public class SqlHolder {
+	/** sql内容 */
 	private String sql;
+	
+	/** 是否使用模板解析sql */
 	private Boolean template;
+	
 	public String getSql() {
 		return sql;
 	}
+	
 	public void setSql(String sql) {
 		this.sql = sql;
 	}
+	
 	public Boolean getTemplate() {
 		return template;
 	}
+	
 	public void setTemplate(Boolean template) {
 		this.template = template;
 	}
