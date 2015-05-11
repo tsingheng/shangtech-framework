bc65c62e68826a168f77cab1a175fd5e8944d34b
songxh <tsingheng@163.com>
2015-05-11 21:04:47 +0800

    add contails tag

--- src/main/java/net/shangtech/framework/web/jsp/taglibs/ShangTechFunctions.java
+++ src/main/java/net/shangtech/framework/web/jsp/taglibs/ShangTechFunctions.java
@@ -1,5 +1,7 @@
 package net.shangtech.framework.web.jsp.taglibs;
 
+import java.util.Collection;
+
 public class ShangTechFunctions {
 	
 	public static int max(int a, int b){
@@ -12,5 +14,20 @@ public class ShangTechFunctions {
 	
 	public static double ceil(double a){
 		return Math.ceil(a);
+	}
+	
+	public static boolean contains(Collection<?> collection, Object object){
+		if(collection == null || collection.isEmpty()){
+			return false;
+		}
+		for(Object item : collection){
+			if(item == null && object == null){
+				return true;
+			}
+			if(item != null && item.equals(object)){
+				return true;
+			}
+		}
+		return false;
 	}
 }
