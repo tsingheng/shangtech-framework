7fe93480dda579a1e97d94476d991400db975649
tsingheng <tsingheng@163.com>
2015-11-14 22:48:56 +0800

    add BaseEntity

---
+++ src/main/java/net/shangtech/framework/dao/BaseEntity.java
@@ -0,0 +1,27 @@
+package net.shangtech.framework.dao;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 8388927255446044313L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
